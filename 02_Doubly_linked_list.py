92a4241e6d703772f27e75bb6c94b26ad4772264
Dharmesh Sharma <contact@dharmeshsharma.me>
2026-03-11 14:38:39 +0530

    Changed the get_length(), print_list() and minor changes in functions
    Introduced self.length variable to keep track of length of the list while inserting and deleting, reducing time complexity of get_length() from O(n) to O(1).
    
    In  functions, print_forward() and print_backward(), instead of using a string variable, directly printing the output, reducing the time complextity to O(n), also reducing space used by string variable.
    
    Minorly changed the each function by covering the logic into if..else statement, using print statement instead of raise(), removing return statement in some places and introducing elif/else block.

--- 02_Doubly_linked_list.py
+++ 02_Doubly_linked_list.py
@@ -8,85 +8,88 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
     def insert_at_beginning(self, data):
         if self.head is None:
             node = Node(data)
             self.head = node
             self.tail = node
-            return
-        node = Node(data, self.head)
-        self.head.prev = node
-        self.head = node
+
+        else:
+            node = Node(data, self.head)
+            self.head.prev = node
+            self.head = node
+        self.length += 1
 
     def insert_at_end(self, data):
         if self.head is None:
             node = Node(data)
             self.head = node
             self.tail =  node
-            return
-    
-        if self.tail == self.head:
+
+        elif self.tail == self.head:
             node = Node(data, None, self.head)
             self.tail = node
 
-        node = Node(data,None, self.tail)
-        self.tail.next = node
-        self.tail = node
+        else:
+            node = Node(data,None, self.tail)
+            self.tail.next = node
+            self.tail = node
+
+        self.length += 1
 
     def get_length(self):
-        count = 0
-        itr = self.head
-        while itr:
-            count += 1
-            itr = itr.next
-        return count
+       return self.length
     
     def insert_at(self, index, data):
         if index < 0 or index > self.get_length():
-            raise Exception("Invalid Index")
+            print("Invallid Index")
         
-        itr = self.head
-        count = 0
-        while itr:
-            if count == index -1:
-                node = Node(data, itr.next, itr)
-                itr.next.prev = node
-                itr.next = node
+        else:
+            itr = self.head
+            count = 0
+            while itr:
+                if count == index -1:
+                    node = Node(data, itr.next, itr)
+                    itr.next.prev = node
+                    itr.next = node
 
             # (OR)
 
-            """
-            if count == index:
-                node = Node(data, itr, itr.prev)
-                itr.prev.next = node
-                itr.prev = node
-            """
+                """
+                if count == index:
+                    node = Node(data, itr, itr.prev)
+                    itr.prev.next = node
+                    itr.prev = node
+                """
 
-            itr = itr.next
-            count += 1
+                itr = itr.next
+                count += 1
 
     
     def remove_at(self, index):
         if index < 0 or index >= self.get_length():
-            raise Exception("Invalid Index")
+            print("Invalid Index")
         
-        if index == 0:
+        elif index == 0:
             self.head = self.head.next
             self.head.prev = None
-            return
+            self.length -= 1
         
-        count = 0
-        itr = self.head
-        while itr:
+        else:
+            count = 0
+            itr = self.head
+            while itr:
 
-            if count == index:
-                itr.prev.next = itr.next
-                if itr.next:
-                    itr.next.prev = itr.prev
+                if count == index:
+                    itr.prev.next = itr.next
+                    if itr.next:
+                        itr.next.prev = itr.prev
 
-            count += 1
-            itr = itr.next
+                count += 1
+                itr = itr.next
+            self.length -= 1
 
     def remove_by_value(self, data):
         itr = self.head
@@ -99,6 +102,7 @@ class DoublyLinkedList:
                     self.tail = itr.prev
                 return           
             itr = itr.next
+        self.length -= 1
 
     def insert_list(self, data_list):
         for data in data_list:
@@ -108,26 +112,26 @@ class DoublyLinkedList:
     def print_forward (self):
         if self.head is None:
             print("The doubly Linked list is empty")
-            return
-        itr = self.head
-        str = 'none<-->'
-        while itr:
-            str += f'{itr.data}<-->'
-            itr = itr.next
-        str += 'none'
-        print(str)
+            
+        else:
+            itr = self.head
+            print("none", end='<-->')
+            while itr:
+                print(itr.data, end='<-->')
+                itr = itr.next
+            print('none')
 
     def print_backward (self):
         if self.head is None:
             print("The doubly linked list is empty")
-            return
-        itr = self.tail
-        str = 'none<-->'
-        while itr:
-            str += f'{itr.data}<-->'
-            itr = itr.prev
-        str += 'none'
-        print(str)
+
+        else:
+            itr = self.tail
+            print('none', end='<-->')
+            while itr:
+                print(itr.data, end='<-->')
+                itr = itr.prev
+            print('none')
     
 dll = DoublyLinkedList()
 dll.insert_at_beginning(2)
